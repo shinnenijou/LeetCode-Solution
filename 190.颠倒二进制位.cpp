f4b62440f79296d60e16f4e68194f816fd971acc
Shinnenijou <littlesword111@gmail.com>
2022-08-03 15:18:47 +0800

    Initial commit

---
+++ 190.颠倒二进制位.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=190 lang=cpp
+ *
+ * [190] 颠倒二进制位
+ */
+
+// @lc code=start
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t masks[2] = {0, 0xFFFFFFFF};
+        uint32_t bit = 1, ret = 0;
+    }
+};
+// @lc code=end
+
