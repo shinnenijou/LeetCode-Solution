71f1e1b027d23aedad13fda46fbaea4c92595eef
Shinnenijou <littlesword111@gmail.com>
2022-09-07 12:15:03 +0800

    Update 589.n-叉树的前序遍历.cpp
    38/38 cases passed (16 ms)
    Your runtime beats 69.63 % of cpp submissions
    Your memory usage beats 67.7 % of cpp submissions (11.2 MB)

--- 589.n-叉树的前序遍历.cpp
+++ 589.n-叉树的前序遍历.cpp
@@ -24,22 +24,53 @@ public:
     }
 };
 */
-
-class Solution {
-private:
-    void _preorder(Node *root, vector<int> &ivec)
-    {
-        if(!root) return;
-        ivec.push_back(root->val);
-        for(int i = 0; i != root->children.size(); i++){
-            _preorder((root->children)[i], ivec);
-        }
-    }
-
+#include <vector>
+#include <stack>
+using std::stack;
+using std::vector;
+class Solution
+{
 public:
-    vector<int> preorder(Node* root) {
+    vector<int> preorder(Node *root)
+    {
         vector<int> ret;
-        _preorder(root, ret);
+        if (!root)
+            return ret;
+
+        stack<int> child_index;
+        stack<Node *> parent;
+        Node *cur_node = root;
+
+        while (!parent.empty() || ret.empty())
+        {
+            ret.push_back(cur_node->val);
+            if (!cur_node->children.empty())
+            {
+                parent.push(cur_node);
+                child_index.push(0);
+                cur_node = cur_node->children.front();
+            }
+            else
+            {
+                while (!parent.empty())
+                {
+                    if (child_index.top() + 1 >= parent.top()->children.size())
+                    {
+                        child_index.pop();
+                        parent.pop();
+                    }
+                    else
+                    {
+                        int temp = child_index.top();
+                        child_index.pop();
+                        child_index.push(temp + 1);
+                        cur_node = parent.top()->children[temp + 1];
+                        break;
+                    }
+                }
+            }
+        }
+
         return ret;
     }
 };
