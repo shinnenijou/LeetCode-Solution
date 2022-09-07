8643dcdab1dd5dacbe09f6d55eb5dd43fc7da5f9
Shinnenijou <littlesword111@gmail.com>
2022-09-07 16:15:56 +0800

    Create 598.范围求和-ii.cpp
    69/69 cases passed (4 ms)
    Your runtime beats 99.69 % of cpp submissions
    Your memory usage beats 61.47 % of cpp submissions (10.7 MB)

---
+++ 598.范围求和-ii.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=598 lang=cpp
+ *
+ * [598] 范围求和 II
+ */
+#include<climits>
+#include<vector>
+// @lc code=start
+class Solution {
+public:
+    int maxCount(int m, int n, vector<vector<int>>& ops) {
+        int ret = m * n;
+        int min_a = INT_MAX, min_b = INT_MAX;
+        for(int i = 0; i != ops.size(); i++){
+            min_a = ops[i][0] < min_a ? ops[i][0] : min_a;
+            min_b = ops[i][1] < min_b ? ops[i][1] : min_b;
+        }
+        if(min_a != INT_MAX && min_b != INT_MAX) ret = min_a * min_b;
+        return ret;
+    }
+};
+// @lc code=end
+
