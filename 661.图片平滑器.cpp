5ab1be5adbb942d66fa8682dc15e05203711b113
Shinnenijou <littlesword111@gmail.com>
2022-09-08 16:29:27 +0800

    Update 661.图片平滑器.cpp
    203/203 cases passed (40 ms)
    Your runtime beats 93.1 % of cpp submissions
    Your memory usage beats 32.76 % of cpp submissions (21.2 MB)

--- 661.图片平滑器.cpp
+++ 661.图片平滑器.cpp
@@ -35,10 +35,24 @@ public:
     {
         int n = img.size(), m = img.front().size();
         vector<vector<int>> smooth_img(n, vector<int>(m));
+        for (int i = 1; i < n - 1; i++)
+        {
+            for (int j = 1; j < m - 1; j++)
+            {
+                smooth_img[i][j] = _average_gray_scale(
+                    img, i - 1, i + 1, j - 1, j + 1
+                );
+            }
+        }
+
         for (int i = 0; i != n; i++)
         {
             for (int j = 0; j != m; j++)
             {
+                if(i > 0 && i < n - 1 && j > 0 && j < m - 1){
+                    continue;
+                }
+
                 smooth_img[i][j] = _average_gray_scale(
                     img, 
                     _clamp(i - 1, 0, n - 1), 
@@ -48,6 +62,7 @@ public:
                 );
             }
         }
+
         return smooth_img;
     }
 };
