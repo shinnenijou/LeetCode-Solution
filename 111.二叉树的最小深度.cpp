e1fd7b4937c9a0e45d05d87ba06b4cb3cdfd82a6
Shinnenijou <littlesword111@gmail.com>
2022-09-21 18:05:37 +0800

    Update 111.二叉树的最小深度.cpp
    玄学用时
    52/52 cases passed (220 ms)
    Your runtime beats 74.9 % of cpp submissions
    Your memory usage beats 17.81 % of cpp submissions (141.4 MB)

--- 111.二叉树的最小深度.cpp
+++ 111.二叉树的最小深度.cpp
@@ -22,7 +22,6 @@ class Solution {
 public:
     int minDepth(TreeNode* root) {
         if(!root) return 0;
-        if(!root->left && !root->right) return 1;
         if(!root->left) return minDepth(root->right) + 1;
         if(!root->right) return minDepth(root->left) + 1;
         return (std::min(minDepth(root->left), minDepth(root->right))) + 1;
